9c4ba0a461073d4e6aae03e757328b773a84dd50
Daniel Hoyos <danielhoyos350@gmail.com>
2019-08-07 22:02:39 -0500

    Updates

---
+++ precache-manifest.a7b90344fef050b68000039d3efe1f18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3dbc06e451211e7c84867c73797666",
+    "url": "/gastosemanal/index.html"
+  },
+  {
+    "revision": "e2226c906ed0f1cf1e91",
+    "url": "/gastosemanal/static/css/main.c012dbc9.chunk.css"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/gastosemanal/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "e2226c906ed0f1cf1e91",
+    "url": "/gastosemanal/static/js/main.334d99fb.chunk.js"
+  },
+  {
+    "revision": "8b6a4e098b7006de9a65",
+    "url": "/gastosemanal/static/js/runtime~main.948489c5.js"
+  }
+]);
